584aaf3b40759aa6588dfdca323c556fc638ef6d
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-24 12:16:10 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
     <div >
-     <h1 className='text-3xl font-bold underline'>
-      Hello world
-     </h1>
+     <Header></Header>
     </div>
   )
 }
