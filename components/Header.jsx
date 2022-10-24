65088d32dae4d7e7717d74aafcf942ffad616f2c
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-24 13:06:29 +0200

    update

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className='bg-gray-50 px-5'>
         <div className='xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3'>
             <div className='md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0'>
-                <input className='mt-1 block w-60 px-3 py-2 bg-white border borders-slate-300 rounded-full text-s, shadow-sm placehoder-slate-500' type="text" placeholder="Search..." />
+                <input className='input-text placeholder-slate-500' type="text" placeholder="Search..." />
             </div>
             <div className='shrink w-80 sm:order-2 '>
                 <a>Design</a>
