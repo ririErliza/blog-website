27a368dbd2a48fb7958aee8e6d489162c38b3e54
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-25 11:23:53 +0200

    update

--- components/Section1.jsx
+++ components/Section1.jsx
@@ -16,18 +16,30 @@ export default function Section1() {
   )
 }
 
+{/* <Link href={"/"}><a ></a></Link> */}
+              
+
 function Slide(){
     return (
         <div className='grid md:grid-cols-2'>
             <div className='image'>
-                <Link href={"/"}><a> <Image src={"/images/9.jpg"} width={600} height={600} layout="responsive" alt=""/></a></Link>
+                <Link href={"/"}><a> <Image src={"/images/9.jpg"} width={600} height={600} alt=""/></a></Link>
                 
 
             </div>
-            <div className="info">
+            <div className="info flex justify-center flex-col">
                 <div className="category">
-                    <Link href=""><a ></a></Link>
+                    <Link href={"/"}><a className='text-orange-600 hover:text-orange-800'> Business, Travel</a></Link>
+                    <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
                 </div>
+                <div className="title">
+                    <Link href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
+                </div>
+                <p className="text-gray-500 py-3">
+                    Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
+                    text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+                </p>
+                <h1>author</h1>
 
             </div>
         </div>
