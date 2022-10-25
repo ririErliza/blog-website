e0feb3149bd987e135294df8bbf1bae4fa44b62a
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-25 12:18:31 +0200

    update

--- components/Section1.jsx
+++ components/Section1.jsx
@@ -1,18 +1,39 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import Author from './_child/Author'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function Section1() {
+    const bg = {
+        background: "url('/images/banner.png') no-repeat",
+        // backgroundPosition: "right"
+    }
+
   return (
-    <section className='py-16'>
-        <div className='container mx-auto md:px-20'>
-            <h1 className='font-bold text-4xl pb-12 text-center'>Trending</h1>
+    <section className="py-16" style={bg}>
+    <div className="container mx-auto md:px-20">
+        <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
 
-        {Slide()}
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+            >
+            <SwiperSlide> { Slide() }</SwiperSlide>
+            <SwiperSlide> { Slide() }</SwiperSlide>
+            <SwiperSlide> { Slide() }</SwiperSlide>
+            <SwiperSlide> { Slide() }</SwiperSlide>
+        ...
+        </Swiper>
 
-        </div>
+       
 
-    </section>
+
+    </div>
+</section>
   )
 }
 
@@ -21,26 +42,23 @@ export default function Section1() {
 
 function Slide(){
     return (
-        <div className='grid md:grid-cols-2'>
-            <div className='image'>
-                <Link href={"/"}><a> <Image src={"/images/9.jpg"} width={600} height={600} alt=""/></a></Link>
-                
-
+        <div className="grid md:grid-cols-2">
+            <div className="image mr-10">
+                <Link href={"/"}><a><Image className='object-cover' src={"/images/1.jpg"} width={600} height={600} alt="image"/></a></Link>
             </div>
             <div className="info flex justify-center flex-col">
-                <div className="category">
-                    <Link href={"/"}><a className='text-orange-600 hover:text-orange-800'> Business, Travel</a></Link>
+                <div className="cat">
+                    <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
                     <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
                 </div>
                 <div className="title">
-                    <Link href={"/"}><a className="text-3xl md:text-6xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
+                    <Link href={"/"}><a className="text-2xl md:text-5xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
                 </div>
                 <p className="text-gray-500 py-3">
                     Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
                     text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
                 </p>
-                <h1>author</h1>
-
+                <Author/>
             </div>
         </div>
     )
---
+++ components/_child/Author.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+{/* <Link href={"/"}><a className=''></a></Link> */}
+
+export default function Author() {
+  return (
+    <div className='author flex py-5'>
+        <Image className='rounded-full object-cover' src={"/images/author/3.jpg"} width={60} height={60} alt="author"></Image>
+        <div className="flex flex-col justify-center px-4">
+            <Link href={"/"}><a className='text-md font-bold text-gray hover:text-gray-600'>Flying High</a></Link>
+            <span className="text-sm text-gray-500">CEO and Founder</span>
+        </div>
+    </div>
+  )
+}
