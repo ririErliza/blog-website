209951c2e99f20e7e76f1122149cd12f06730dd0
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-10-25 11:15:11 +0200

    update

--- layout/Format.jsx
+++ layout/Format.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Head from 'next/head'
 
-export default function Format() {
+
+export default function Format({children}) {
   return (
-    <div>Layout Component</div>
+    <>
+    <Head>
+        <title>Blog </title>
+    </Head>
+
+    <Header/>
+    <main>{children}</main>
+    <Footer/>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
 import Format from '../layout/Format'
-import styles from '../styles/Home.module.css'
 
+//components
+import Section1 from '../components/Section1'
 
 export default function Home() {
   return (
     <Format >
-     <Header/>
-     <main> main component </main>
-     <Footer/>
+      <Section1/>
     </Format>
   )
 }
