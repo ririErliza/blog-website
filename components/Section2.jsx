c5b7dc10ee5757ee9775e83e5dd7c3c7a6b2048a
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-07 12:16:14 +0100

    add post

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -9,7 +9,14 @@ export default function Section2() {
 
 
     {/* grid columns */}
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14"></div>
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
+            { Post() }
+            { Post() }
+            { Post() }
+            { Post() }
+            { Post() }
+            { Post() }
+    </div>
     </section>
   )
 }
@@ -18,10 +25,22 @@ function Post(){
 return(
     <div className="item">
         <div className="images">
-            <Link href={"/"}><a><Image src={"/images/img1.jpg"} className="rounded" width={500} height={350} alt="image"/></a></Link>
+            <Link href={"/"}><a><Image src={"/images/10.jpg"} className="rounded" width={500} height={350} alt="image"/></a></Link>
         </div>
 
         <div className="info flex justify-center flex-col py-4">
+                <div className="cat">
+                    <Link href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
+                    <Link href={"/"}><a className="text-gray-800 hover:text-gray-600">- July 3, 2022</a></Link>
+                </div>
+                <div className="title">
+                    <Link href={"/"}><a className="text-xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source of learning</a></Link>
+                </div>
+                <p className="text-gray-500 py-3">
+                    Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
+                    text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+                </p>
+                <Author/>
 
         </div>
 
