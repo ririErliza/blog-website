184bd2e2636ead1ff1f9a4b6476580a84fc9a9ef
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-07 18:41:41 +0100

    update

--- components/_child/Author.jsx
+++ components/_child/Author.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 export default function Author() {
   return (
     <div className='author flex py-5'>
-        <Image className='rounded-full object-cover' src={"/images/author/3.jpg"} width={60} height={60} alt="author"></Image>
+        <Image className='rounded-full object-cover' src={"/images/author/3.jpg"} width={30} height={30} alt="author"></Image>
         <div className="flex flex-col justify-center px-4">
             <Link href={"/"}><a className='text-md font-bold text-gray hover:text-gray-600'>Flying High</a></Link>
             <span className="text-sm text-gray-500">CEO and Founder</span>
