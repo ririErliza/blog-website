471605c655c7a4019acff0f9c77a5bea7bc6776e
ririErliza <95483807+ririErliza@users.noreply.github.com>
2022-11-07 12:13:04 +0100

    add image

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -1,7 +1,34 @@
-
+import Link from "next/link"
+import Image from "next/image"
+import Author from "./_child/Author"
 
 export default function Section2() {
   return (
-    <div>Section2</div>
+    <section className="container mx-auto md:px-20 py-10">
+        <h1 className="font-bold text-4xl py-12 text-center">Latest Post</h1>
+
+
+    {/* grid columns */}
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14"></div>
+    </section>
   )
+}
+
+function Post(){
+return(
+    <div className="item">
+        <div className="images">
+            <Link href={"/"}><a><Image src={"/images/img1.jpg"} className="rounded" width={500} height={350} alt="image"/></a></Link>
+        </div>
+
+        <div className="info flex justify-center flex-col py-4">
+
+        </div>
+
+
+
+
+    </div>
+)
+
 }
